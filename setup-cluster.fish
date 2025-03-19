c611c4c492b1f901af1ea6092d7b40c5276c13ac
Rafael Pereira <rafael.pereira@cilia.com.br>
2025-03-19 08:54:53 -0300

    WIP: postgre helm default values

--- setup-cluster.fish
+++ setup-cluster.fish
@@ -9,6 +9,14 @@ end
 printf "Setup kind cluster\n"
 kind create cluster --name flink --config conf/kind-config.yaml
 
+if test $status -ne 0
+    printf "Failed to create kind cluster\n"
+    exit 1
+end
 
 printf "Install Ingress Nginx\n"
 kubectl apply -f conf/deploy-ingress-nginx.yaml
+
+printf "Installing PostgreSQL\n\n"
+
+helm install postgre oci://registry-1.docker.io/bitnamicharts/postgresql -n databases --create-namespace --config conf/postgre/postgresql-values.yaml
